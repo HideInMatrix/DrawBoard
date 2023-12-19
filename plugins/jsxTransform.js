47e4396d9da6c942747e08040307d087fb40e520
HideInMatrix <davidmorgancreate@gmail.com>
2023-12-19 09:44:46 +0800

    feat:dom元素添加随机属性值

--- plugins/jsxTransform.js
+++ plugins/jsxTransform.js
@@ -1,8 +1,31 @@
+const types = require("@babel/types");
+const addProperty = () => ({
+  visitor: {
+    JSXOpeningElement(path) {
+      const { name, attributes } = path.node;
+      let flag = !attributes.some(
+        (attr) =>
+          attr.type === "JSXAttribute" &&
+          attr.name.name === "class"
+      )
+      if (flag) {
+        // 添加属性到元素
+        path.node.attributes.push(
+          types.jsxAttribute(
+            types.jsxIdentifier(`data-v-${new Date().getTime()}`),
+            types.stringLiteral("true")
+          )
+        );
+      }
+    },
+  },
+})
 const jsxTransform = () => ({
   name: "jsx-transform",
   setup(build) {
     const fs = require("fs");
     const babel = require("@babel/core");
+
     const plugin = require("@babel/plugin-transform-react-jsx").default(
       {},
       { runtime: "automatic" }
@@ -10,11 +33,11 @@ const jsxTransform = () => ({
     const presetEnv = require('@babel/preset-env');
     const presetTypescript = require('@babel/preset-typescript');
 
-
     build.onLoad({ filter: /\.[j|t]sx$/ }, async (args) => {
       const jsx = await fs.promises.readFile(args.path, "utf8");
-
-      const result = babel.transformSync(jsx, { plugins: [plugin], presets: [presetEnv, presetTypescript], filename: args.path });
+      const result = babel.transformSync(jsx, {
+        plugins: [plugin, addProperty()], presets: [presetEnv, presetTypescript], filename: args.path
+      });
       return { contents: result.code };
     });
   },
